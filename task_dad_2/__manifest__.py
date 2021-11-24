cd2db32e514b5071c4d1e0c06b00d8be92038d15
odoocourse18 <92395581+odoocourse18@users.noreply.github.com>
2021-11-24 19:07:35 +0200

    Add files via upload

---
+++ task_dad_2/__manifest__.py
@@ -0,0 +1,1 @@
+{    'name': 'Task Dadd',    'author': 'Ezz Edin Saleh',    'depends': ['base','sale_management'],    'data': [        'security/ir_rule.xml',        'views/res_partner.xml',    ],}
